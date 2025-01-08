b8b0222cbc829a3c65e84a5ab4cd5f8bfac2655b
shambhavi <sm1066279@gmail.com>
2025-01-08 08:48:25 +0530

    modified

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,41 +1,31 @@
-#os, sys - Handle file paths and system-related tasks
 import os
 import sys
-
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-
-# Create a configuration class (DataIngestionConfig) to manage paths easily.
 from dataclasses import dataclass
 
-#Importing Other pipeline components
-from src.components.data_transformation import DataTransformation,  DataTransformationConfig
-from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
-#When Should You Use @dataclass?
-#--When you need a class to store data rather than behavior.
-#--When you want to avoid boilerplate code for initialization, comparison, or representation.
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
-
 class DataIngestion:
-#Initializes the class when an object is created.
     def __init__(self):
-        #Creates an instance of the DataIngestionConfig class
         self.ingestion_config=DataIngestionConfig()
 
-#Performs the main task of reading, splitting, and saving data.
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\StudentsPerformance.csv')
+            df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
--- src/exception.py
+++ src/exception.py
@@ -2,10 +2,14 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
+   if error_detail.exc_info() is None:
+    error_message = f"Error: {str(error)}"
+   else:
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))
+    error_message="Error occurred in script [{0}] line number [{1}] error [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
 
     return error_message
     
